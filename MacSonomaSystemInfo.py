9d703b2fb33fc74428dca6f8047c887647bf00fb
D_T_Song <90113176+MrCyf3r@users.noreply.github.com>
2024-02-24 12:33:58 -0800

    Add files via upload

---
+++ MacSonomaSystemInfo.py
@@ -0,0 +1,15 @@
+# Quick script for MacOS Sonoma to check for system updates.
+import subprocess
+
+
+def check_for_updates():
+    print("Checking for software updates...")
+    # Use subprocess module to return output instead of exit code.
+    result = subprocess.check_output(['softwareupdate', '-l']).decode('utf-8')
+    if "No new software available" in result:
+        print("No new software available")
+    else:
+        print(result)
+
+
+check_for_updates()
